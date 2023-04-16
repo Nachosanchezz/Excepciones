8ea00f7b1f8f3f49bf13495257e985b924984831
Nacho <isanccal@myuax.com>
2023-04-16 16:07:35 +0200

    Cambio

--- Excepciones.py
+++ Excepciones.py
@@ -6,4 +6,19 @@ class (Correo):
 
     def validar(self):
         valida = re.search(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", self.correo)
-        
+        introducirCorreo = input("->")
+        if valida:
+            if introducirCorreo == self.correo:
+                nombre = ""
+                for i in self.correo:
+                    if i == "@":
+                        break
+                    else:
+                        nombre += i
+                nombre = nombre.capitalize()
+                return "Bienvenido " + nombre
+            else:
+                raise Exception("El correo no coincide")
+        else:
+            return "Correo no valido"
+
